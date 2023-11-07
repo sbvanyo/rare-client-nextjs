1ee0cdb1149978a4a6d9f08b75cffe4fc84467b3
Stacey Vanyo <sbvanyo@gmail.com>
2023-11-06 21:36:14 -0600

    category api calls + card component

---
+++ api calls/category.js
@@ -0,0 +1,69 @@
+const getCategories = () => new Promise((resolve, reject) => {
+  fetch('http://localhost:8088/categories.json', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+const getSingleCategory = (id) => new Promise((resolve, reject) => {
+  fetch(`http://localhost:8088/categories/${id}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const deleteCategory = (id) => new Promise((resolve, reject) => {
+  fetch(`http://localhost:8088/categories/${id}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve((data)))
+    .catch(reject);
+});
+
+const createCategory = (payload) => new Promise((resolve, reject) => {
+  fetch('http://localhost:8088/categories.json', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateCategory = (payload) => new Promise((resolve, reject) => {
+  fetch(`http://localhost:8088/categories/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getCategories,
+  getSingleCategory,
+  createCategory,
+  deleteCategory,
+  updateCategory,
+};
